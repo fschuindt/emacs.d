c883d6f8e65e747b8d3a5b0afed41c9ba92831e3
Fernando Schuindt <f.schuindtcs@gmail.com>
2018-03-06 22:09:25 -0300

    Update Elpa packages

---
+++ elpa/protobuf-mode-20170526.950/protobuf-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "protobuf-mode" "20170526.950" "major mode for editing protocol buffers." 'nil :commit "a6037c5230c0ccb6531f80cef909dbfe37257884" :keywords '("google" "protobuf" "languages"))
