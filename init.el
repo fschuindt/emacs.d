39a38b12c01bc582040cf10c0d2cab56dc464d5a
Fernando Schuindt <f.schuindtcs@gmail.com>
2020-04-05 19:51:38 -0300

    feat: Add LSP and other tricks

--- init.el
+++ init.el
@@ -19,6 +19,20 @@
 ;; Support Brazilian ABNT-2 keyboard layout
 (require 'iso-transl)
 
+;; Splash Image
+(setq fancy-splash-image "~/.emacs.d/gnu_splash.png")
+
+;; yasnippet
+(yas-global-mode)
+
+;; use-package
+(eval-when-compile
+  (require 'use-package))
+
+;; Company LSP
+(require 'company-lsp)
+(push 'company-lsp company-backends)
+
 ;; Use ls-lisp instead ot the system's ls.
 ;; Sort directories to be shown first in Dired mode.
 (require 'ls-lisp)
@@ -32,7 +46,7 @@
 ;; Disable UTF-8 file tags
 (setq ruby-insert-encoding-magic-comment nil)
 
-;; Kill all buffers
+;; Kill all buffers fuction
 (defun buffer-reset ()
   (interactive)
   (mapc 'kill-buffer (buffer-list)))
@@ -40,7 +54,88 @@
 ;; Disable auto save files
 (setq auto-save-default nil)
 
-;; Manage Emacs backup files
+;; Show line numbers
+(global-linum-mode t)
+(setq linum-format " %d  ")
+
+;; Default font
+(set-frame-font "Inconsolata 19" nil t)
+
+;; -------------------------------------
+;; TypeScript - BEGIN
+;; -------------------------------------
+
+(defun setup-tide-mode ()
+  (interactive)
+  (tide-setup)
+  (flycheck-mode +1)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  (eldoc-mode +1)
+  (tide-hl-identifier-mode +1)
+  ;; company is an optional dependency. You have to
+  ;; install it separately via package-install
+  ;; `M-x package-install [ret] company`
+  (company-mode +1))
+
+;; aligns annotation to the right hand side
+(setq company-tooltip-align-annotations t)
+
+;; formats the buffer before saving
+;; (add-hook 'before-save-hook 'tide-format-before-save)
+
+(add-hook 'typescript-mode-hook #'setup-tide-mode)
+
+;; set 2 spaces instead of TABs.
+(setq js-indent-level 2)
+(setq typescript-indent-level 2)
+
+(use-package tide
+  :ensure t
+  :after (typescript-mode company flycheck)
+  :hook ((typescript-mode . tide-setup)
+         (typescript-mode . tide-hl-identifier-mode)
+         ;; (before-save . tide-format-before-save)
+         ))
+
+;; -------------------------------------
+;; TypeScript - END
+;; -------------------------------------
+
+;; -------------------------------------
+;; LSP - BEGIN
+;; -------------------------------------
+
+(use-package lsp-mode
+    :commands lsp
+    :ensure t
+    :diminish lsp-mode
+    :hook
+    (elixir-mode . lsp)
+    (dart-mode . lsp)
+    :init
+    (add-to-list 'exec-path "/home/fschuindt/apps/clones/elixir-ls/rel"))
+
+(defvar lsp-elixir--config-options (make-hash-table))
+(puthash "dialyzerEnabled" :json-false lsp-elixir--config-options)
+(add-hook 'lsp-after-initialize-hook
+          (lambda ()
+            (lsp--set-configuration `(:elixirLS, lsp-elixir--config-options))))
+
+(use-package lsp-ui :commands lsp-ui-mode)
+(use-package company-lsp :commands company-lsp)
+(use-package helm-lsp :commands helm-lsp-workspace-symbol)
+
+;; To use debugger
+(use-package dap-mode)
+
+;; -------------------------------------
+;; LSP - END
+;; -------------------------------------
+
+;; -------------------------------------
+;; Backup Files - BEGIN
+;; -------------------------------------
+
 (setq backup-directory-alist `(("." . "~/.emacs_backups")))
 
 (setq backup-by-copying t)
@@ -50,12 +145,18 @@
   kept-old-versions 2
   version-control t)
 
-;; Show line numbers
-(global-linum-mode t)
-(setq linum-format " %d  ")
+;; -------------------------------------
+;; Backup Files - END
+;; -------------------------------------
 
-;; Default font
-(set-frame-font "Inconsolata 19" nil t)
+;; -------------------------------------
+;; Indent - BEGIN
+;; -------------------------------------
+
+;; Use TABs for C
+(setq-default c-basic-offset 4
+			  tab-width 4
+              indent-tabs-mode t)
 
 ;; Use spaces to indent function
 (defun soft-tabs ()
@@ -63,18 +164,20 @@
   (setq-default indent-tabs-mode nil)
 )
 
-;; Use TABs to indent function
-(defun hard-tabs ()
-  (interactive)
-  (setq-default c-basic-offset 4
-				tab-width 4
-                indent-tabs-mode t)
-)
-
 ;; Default to soft-tabs
 (soft-tabs)
 
 (setq highlight-indentation-mode t)
+
+;; 2 spaces for CSS
+(setq css-indent-offset 2)
+
+;; 2 spaces for JS
+(setq js-indent-level 2)
+
+;; -------------------------------------
+;; Indent - END
+;; -------------------------------------
 
 ;; -------------------------------------
 ;; Show Invisibles - BEGIN
@@ -133,16 +236,18 @@
 
   ;; Column at line 80' color
   (setq fci-rule-color "#444444")
-
 )
+
+;; Set whitefy() as main theme script
+(whitefy)
 
 ;; -------------------------------------
 ;; Blackfy and Whitefy - END
 ;; -------------------------------------
 
-;; -----------------------------
+;; -------------------------------------
 ;; Column at line 80' - BEGIN
-;; -----------------------------
+;; -------------------------------------
 
 (setq fci-rule-width 1)
 
@@ -169,13 +274,13 @@
   (fci-mode 1)
 ))
 
-;; -----------------------------
+;; -------------------------------------
 ;; Column at line 80' - END
-;; ----------------------------
+;; -------------------------------------
 
-;; -----------------------------
+;; -------------------------------------
 ;; Start diff-hl-mode - BEGIN
-;; ----------------------------
+;; -------------------------------------
 
 (add-hook 'prog-mode-hook (lambda ()
   (diff-hl-mode 1)
@@ -191,9 +296,13 @@
   (dired-hide-details-mode 1)
 ))
 
-;; -----------------------------
+;; -------------------------------------
 ;; Start diff-hl-mode - END
-;; ----------------------------
+;; -------------------------------------
+
+;; -------------------------------------
+;; Helm and Projectile - BEGIN
+;; -------------------------------------
 
 ;; Enables Projectile
 (require 'projectile)
@@ -207,11 +316,9 @@
 ;; Open dired-mode when switching projects
 (setq projectile-switch-project-action 'projectile-dired)
 
-;; Set whitefy() as main theme script
-(whitefy)
-
-;; Splash Image
-(setq fancy-splash-image "~/.emacs.d/gnu_splash.png")
+;; -------------------------------------
+;; Helm and Projectile - END
+;; -------------------------------------
 
 ;; custom-set-variables
 (custom-set-variables
@@ -222,12 +329,13 @@
  '(custom-safe-themes
    (quote
     ("e068203104e27ac7eeff924521112bfcd953a655269a8da660ebc150c97d0db8" default)))
- ;; '(initial-buffer-choice "~/.emacs_welcome")
  '(menu-bar-mode nil)
  '(package-selected-packages
    (quote
-    (multiple-cursors toml-mode feature-mode writeroom-mode helm-ag protobuf-mode dockerfile-mode rust-mode exec-path-from-shell vmd-mode flatui-theme all-the-icons neotree elixir-mode helm-projectile projectile highlight-indentation blank-mode yaml-mode diff-hl markdown-mode fill-column-indicator highlight-indent-guides hl-anything highlight-chars color-theme-sanityinc-tomorrow)))
- '(tool-bar-mode nil))
+    (company tide typescript-mode langtool gnuplot-mode yasnippet use-package dap-mode company-lsp lsp-ui flycheck helm-lsp lsp-mode flutter dart-mode terraform-mode json-reformatter-jq nginx-mode ac-alchemist ac-c-headers ac-clang ac-html ac-html-bootstrap ac-php alchemist php-mode auto-complete wakatime-mode multiple-cursors toml-mode feature-mode writeroom-mode helm-ag protobuf-mode dockerfile-mode rust-mode exec-path-from-shell vmd-mode flatui-theme elixir-mode helm-projectile projectile highlight-indentation blank-mode yaml-mode diff-hl markdown-mode fill-column-indicator highlight-indent-guides hl-anything highlight-chars color-theme-sanityinc-tomorrow)))
+ '(tool-bar-mode nil)
+ '(wakatime-cli-path "/usr/bin/wakatime")
+ '(wakatime-python-bin nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
